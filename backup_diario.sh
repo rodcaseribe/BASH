37c623b753f0ec480ee818e05275424a00b60791
rodcaseribeiro <rodolfo2804@hotmail.com>
2015-11-10 08:38:39 -0200

    Update backup_diario.sh

--- backup_diario.sh
+++ backup_diario.sh
@@ -6,7 +6,7 @@ then
         echo -e "Inicializando Backup Diario em Segunda Feira\n" "$log_dia"
         echo segunda
         #mv PASTADOMINGO/* /PASTASEGUNDA
-        #rm -rf PASTADOMINGO
+        #rm -rf PASTADOMINGO/*
         #tar -cvzf /backup/BackupComprimido$(date +%Y_%m_%d).tar.gz /Pasta_a_ser_comprimida >> /saida.log
 fi
 
@@ -43,7 +43,7 @@ then
         echo -e "Inicializando Backup Diario em Sexta Feira\n" "$log_dia"
         echo sexta
         #mv PASTAQUINTA/* /PASTASEXTA
-        #rm -rf PASTAQUINTA
+        #rm -rf PASTAQUINTA/*
         #tar -cvzf /backup/BackupComprimido$(date +%Y_%m_%d).tar.gz /Pasta_a_ser_comprimida >> /saida.log
 fi
 
@@ -52,7 +52,7 @@ then
         echo -e "Inicializando Backup Diario em Sabado\n" "$log_dia"
         echo sabaado
         #mv PASTASEXTA/* /PASTASABADO
-        #rm -rf PASTASEXTA
+        #rm -rf PASTASEXTA/*
         #tar -cvzf /backup/BackupComprimido$(date +%Y_%m_%d).tar.gz /Pasta_a_ser_comprimida >> /saida.log
 fi
 
@@ -61,7 +61,7 @@ then
         echo -e "Inicializando Backup Diario em Domingo" "$log_dia"
         echo domingo
         #mv PASTASABADO/* /PASTADOMINGO
-        #rm -rf PASTASABADO
+        #rm -rf PASTASABADO/*
         #tar -cvzf /backup/BackupComprimido$(date +%Y_%m_%d).tar.gz /Pasta_a_ser_comprimida >> /saida.log
 
 else
